62b0a095e9bf136d9d03cccb3df069288df8dc66
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2020-12-16 15:32:21 +0000

    used case statement to refactor code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,9 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    'fizzbuzz'
-  elsif number % 3 == 0
-    'fizz'
-  elsif number % 5 == 0
-    'buzz'
-  elsif number.is_a?(String)
-    "That's not an integer"
-  elsif number.is_a?(Float)
-    "That's not an integer"
+  case
+  when number % 15 == 0 then 'fizzbuzz'
+  when number % 3 == 0 then 'fizz'
+  when number % 5 == 0 then 'buzz'
+  when number.is_a?(String) then "That's not an integer"
+  when number.is_a?(Float) then "That's not an integer"
   end
 end
